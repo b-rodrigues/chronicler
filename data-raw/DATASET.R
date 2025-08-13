6d0508798f1d12167d0c6be6b5730ee3cbebddee
Bruno Rodrigues <brodriguesco@protonmail.com>
2025-08-13 12:18:05 +0200

    Update DATASET.R

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,3 +4,6 @@ avia <- readr::read_tsv("~/Downloads/avia_par_lu.tsv") %>%
   as_tibble()
 
 usethis::use_data(avia, overwrite = TRUE)
+
+#avia <- readr::read_tsv("https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/avia_par_lu?format=tsv&compressed=false") %>%
+#  filter(!grepl("^A.*", `freq,unit,tra_meas,airp_pr\\TIME_PERIOD`))
